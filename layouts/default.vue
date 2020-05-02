8ca20e11c8c31132ee3cb641cd2cb1ff9edc32e8
Gerardo Junior <me@gerardo-junior.com>
2020-05-02 05:48:28 -0300

    Create github page

--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-app dark>
-    <h1>Site fora do ar, com problemas técnicos. entre em contato: <a 
-      href="mailto:contact@gerardo-junior.com" 
-      style="color:#fff">contact@gerardo-junior.com</a></h1>
-  </v-app>
-</template>
